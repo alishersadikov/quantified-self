8d7672278ba77c3548a796b5832b799787fcef5f
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-01 18:52:44 -0700

    complete foods sorting test

--- test/sorting-integration-test.js
+++ test/sorting-integration-test.js
@@ -5,7 +5,7 @@ var test      = require('selenium-webdriver/testing');
 
 test.describe('sorting', function() {
   var driver;
-  this.timeout(10000);
+  this.timeout(10000000);
 
   test.beforeEach(function() {
     driver = new webdriver.Builder()
@@ -17,7 +17,7 @@ test.describe('sorting', function() {
     driver.quit();
   })
 
-  test.it('clicking food calories header sorts foods by calories', function() {
+  test.it('clicking food calories header the first time sorts foods by calories in ascending order', function() {
 
     // CREATE FOODS
 
@@ -27,22 +27,22 @@ test.describe('sorting', function() {
     var calories = driver.findElement({id: 'new-food-calories'});
     var submitButton = driver.findElement({id: 'new-submit'});
 
+    name.sendKeys("food2");
+    calories.sendKeys("222");
+    submitButton.click();
 
     name.sendKeys("food1");
     calories.sendKeys("111");
-    submitButton.click();
-
-    name.sendKeys("food2");
-    calories.sendKeys("222");
     submitButton.click();
 
     name.sendKeys("food3");
     calories.sendKeys("333");
     submitButton.click();
 
-
     driver.get('http://localhost:8080');
     driver.sleep(1000)
+
+    // clicking calories the first time sorts in ascending order
 
     var foodCalories = driver.findElement({css: "#food-table > thead > tr > th:nth-child(3)"});
     foodCalories.click();
@@ -70,43 +70,119 @@ test.describe('sorting', function() {
     driver.findElement({css: '#food-table-body tr:nth-child(3) td:nth-child(3)'}).getText().then(function(textValue) {
       assert.equal(textValue, "333");
     });
+  });
+
+  test.it('clicking food calories header the second time sorts foods by calories in descending order', function() {
+
+    // CREATE FOODS
+
+    driver.get('http://localhost:8080/foods.html');
+
+    var name = driver.findElement({id: 'new-food-name'});
+    var calories = driver.findElement({id: 'new-food-calories'});
+    var submitButton = driver.findElement({id: 'new-submit'});
+
+    name.sendKeys("food2");
+    calories.sendKeys("222");
+    submitButton.click();
+
+    name.sendKeys("food1");
+    calories.sendKeys("111");
+    submitButton.click();
+
+    name.sendKeys("food3");
+    calories.sendKeys("333");
+    submitButton.click();
 
 
-    // CREATE EXERCISES
-    //
-    // driver.get('http://localhost:8080/exercises.html');
-    // var name = driver.findElement({id: 'new-exercise-name'});
-    // var calories = driver.findElement({id: 'new-exercise-calories'});
-    // var submitButton = driver.findElement({id: 'new-submit'});
-    //
-    // name.sendKeys('exercise 1');
-    // calories.sendKeys('100');
-    // submitButton.click();
-    //
-    // name.sendKeys('exercise 2');
-    // calories.sendKeys('200');
-    // submitButton.click();
-    //
-    // // Add foods to breakfast lunch and dinner and snacks
-    //
-    // // Select foods and add them to table
-    // driver.get('http://localhost:8080/index.html');
-    //
-    // var foodOneCheckbox = driver.findElement({css: "tbody#food-table-body > tr:nth-child(1) td:nth-child(1) > input[type='checkbox']"});
-    // var foodTwoCheckbox = driver.findElement({css: "tbody#food-table-body > tr:nth-child(2) td:nth-child(1) > input[type='checkbox']"});
-    // foodOneCheckbox.click();
-    // foodTwoCheckbox.click();
-    //
-    // var addToBreakFastButton = driver.findElement({css: "button#add-to-breakfast.btn-info.btn-sm"});
-    // addToBreakFastButton.click();
-    // driver.sleep(10000000)
-    // // debugger;
-    //
-    // driver.findElement({id: 'breakfast-tbody'}).getText().then(function(textValue) {
-    //   assert.include(textValue, "food 1");
-    //   assert.include(textValue, "food 2");
-    //   assert.include(textValue, "100");
-    //   assert.include(textValue, "200");
-    // });
+    driver.get('http://localhost:8080');
+    driver.sleep(1000)
+
+    var foodCalories = driver.findElement({css: "#food-table > thead > tr > th:nth-child(3)"});
+    foodCalories.click();
+    driver.sleep(1000);
+    foodCalories.click();
+
+    driver.findElement({css: '#food-table-body tr:nth-child(1) td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "food3");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(1) td:nth-child(3)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "333");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(2) td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "food2");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(2) td:nth-child(3)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "222");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(3) td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "food1");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(3) td:nth-child(3)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "111");
+    });
+  });
+
+  test.it('clicking food calories header the third time sorts foods by calories in original order', function() {
+
+    // CREATE FOODS
+
+    driver.get('http://localhost:8080/foods.html');
+
+    var name = driver.findElement({id: 'new-food-name'});
+    var calories = driver.findElement({id: 'new-food-calories'});
+    var submitButton = driver.findElement({id: 'new-submit'});
+
+    name.sendKeys("food2");
+    calories.sendKeys("222");
+    submitButton.click();
+
+    name.sendKeys("food1");
+    calories.sendKeys("111");
+    submitButton.click();
+
+    name.sendKeys("food3");
+    calories.sendKeys("333");
+    submitButton.click();
+
+
+    driver.get('http://localhost:8080');
+    driver.sleep(1000);
+
+    var foodCalories = driver.findElement({css: "#food-table > thead > tr > th:nth-child(3)"});
+    foodCalories.click();
+    driver.sleep(1000);
+    foodCalories.click();
+    driver.sleep(1000);
+    foodCalories.click();
+
+    driver.findElement({css: '#food-table-body tr:nth-child(1) td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "food3");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(1) td:nth-child(3)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "333");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(2) td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "food1");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(2) td:nth-child(3)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "111");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(3) td:nth-child(2)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "food2");
+    });
+
+    driver.findElement({css: '#food-table-body tr:nth-child(3) td:nth-child(3)'}).getText().then(function(textValue) {
+      assert.equal(textValue, "222");
+    });
   });
 });
